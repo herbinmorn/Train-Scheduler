c51b36f8bfbf29e2cbf834ef079f0b08cbb782ee
Xiaoquan Zhou <xiaoquan.zhou@email.edcc.edu>
2019-07-06 15:07:43 -0700

    initialize all the files

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyD3IXF0FunsNtI03SisUEIv24MvO3UfITc",
+        authDomain: "herbinmorn-070619.firebaseapp.com",
+        databaseURL: "https://herbinmorn-070619.firebaseio.com",
+        projectId: "herbinmorn-070619",
+        storageBucket: "",
+        messagingSenderId: "740316620897",
+        appId: "1:740316620897:web:32d61899062cee48"
+          };
+        
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
+
