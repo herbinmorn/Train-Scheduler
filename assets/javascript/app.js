642b82957881a349415a425c682738545ee44306
Xiaoquan Zhou <xiaoquan.zhou@email.edcc.edu>
2019-07-12 14:12:57 -0700

    final version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         storageBucket: "",
         messagingSenderId: "740316620897",
         appId: "1:740316620897:web:32d61899062cee48"
-          };
+    };
         
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
@@ -17,19 +17,48 @@ $(document).ready(function() {
 
     $('#submit-btn').on('click',function(event){
         event.preventDefault();
+        
         var dataInputs = {
             trainName:$('#trainName').val(),
             destination:$('#destination').val(),  
             firstTrainTime:$('#firstTrainTime').val(),
-            frequency:$('#frequency').val()
+            frequency:$('#frequency').val(),
         };
         database.ref().push(dataInputs);
     });
+
+    
    
     function displayFirebaseValues(){
         database.ref().on('child_added',function(snapshot){
+            console.log("*********************");
+            var tFrequency = snapshot.val().frequency ;
+            var firstTrainTime = snapshot.val().firstTrainTime ;
+            console.log(firstTrainTime);
+            var convertedFirstTrain = moment(firstTrainTime,"hh:mm");
+            console.log(convertedFirstTrain);
+            var currentTime = moment();
+            console.log(moment(currentTime).format('hh:mm'));
+            var diffTime = moment().diff(moment(convertedFirstTrain),'minutes');
+            console.log('difference in time: '+ diffTime);
+            var tRemainder = diffTime % tFrequency;
+            console.log(tRemainder);
+            var tMinutesTillTrain = tFrequency - tRemainder;
+            console.log(tMinutesTillTrain+' remaining.');
+    
+            var nextTrain = moment().add(tMinutesTillTrain,"minutes");        
+            var nextTrainConverted = moment(nextTrain).format("HH:mm");
+            console.log(nextTrainConverted);
+    
             $('#show-firebase-info').append(
-               '<tr>' + '<td>' + snapshot.val().trainName + '</td>' + '<td>'+ snapshot.val().destination +'</td>'+ '<td>'+ snapshot.val().firstTrainTime +'</td>'+ '<td>'+ snapshot.val().frequency +'</td>'+ '</tr>'
+               '<tr>' + 
+               '<td>' + snapshot.val().trainName + '</td>' + 
+               '<td>' + snapshot.val().destination +'</td>'+ 
+               //'<td>' + snapshot.val().firstTrainTime +'</td>'+ 
+               '<td>' + snapshot.val().frequency +'</td>'+ 
+               '<td>' + nextTrainConverted +'</td>'+ 
+               '<td>' + tMinutesTillTrain +'</td>'+ 
+               '</tr>'
             );
         
         });
